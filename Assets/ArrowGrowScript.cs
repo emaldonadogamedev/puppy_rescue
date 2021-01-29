ebea3579c8a5e5eb1dc9abfc7e5e7f02399011c7
emaldonadogamedev <em_gamedev@outlook.com>
2021-01-28 22:37:20 -0800

    Added arrow that points to where the kart should go, need to update it correctly

---
+++ Assets/ArrowGrowScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGrowScript : MonoBehaviour
+{
+    public float intensity = 1.0f;
+    public float lowerScaleVal = .5f;
+    public float higherScaleVal = 1.3f;
+
+    private Vector3 scale = new Vector3(1,1,1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float v = Mathf.Abs(Mathf.Sin(Time.time * intensity));
+        float v = Mathf.Lerp(lowerScaleVal, higherScaleVal, Mathf.Abs(Mathf.Sin(Time.time * intensity)));
+
+        scale.Set(v, v, v);
+        this.transform.localScale = scale;
+    }
+}
