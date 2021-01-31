9632ef9b7283b90054c6a003bfe5496648435b36
emaldonadogamedev <em_gamedev@outlook.com>
2021-01-30 17:47:09 -0800

    UI now updates with clock

---
+++ Assets/Scripts/Esteban/TimerTextUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerTextUpdate : MonoBehaviour
+{
+    Text uiText;
+    TimeManager timeManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiText = gameObject.GetComponent<Text>();
+        timeManager = FindObjectOfType<TimeManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiText.text = $"{Mathf.RoundToInt(timeManager.TimeRemaining)}";
+    }
+}
