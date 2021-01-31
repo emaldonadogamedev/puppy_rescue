46a188f37dc52e0d4b5f072c1968f2da6a354d27
emaldonadogamedev <em_gamedev@outlook.com>
2021-01-30 23:01:08 -0800

    a bit of an improvement

--- Assets/Scripts/Esteban/ReadTotalDogsRescued.cs
+++ Assets/Scripts/Esteban/ReadTotalDogsRescued.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ReadTotalDogsRescued : MonoBehaviour
 {
     Text uiText;
+    public bool updateTheText = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,9 @@ public class ReadTotalDogsRescued : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(updateTheText)
+        {
+            uiText.text = $"{PerritoGameManager.totalRescuedDogs}";
+        }
     }
 }
